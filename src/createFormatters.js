b8342bbf224f8c7e0f00c868f712cdd0a75f217b
Pavel Lang <langpavel@phpskelet.org>
2016-01-14 01:03:38 +0100

    Records now have a preview

--- src/createFormatters.js
+++ src/createFormatters.js
@@ -1,7 +1,9 @@
-const listStyle = {style: 'list-style-type: none; padding: 0; margin: 0 0 0 12px'};
+const listStyle = {style: 'list-style-type: none; padding: 0; margin: 0 0 0 12px; font-style: normal'};
 const immutableNameStyle = {style: 'color: rgb(232,98,0)'};
-const keyStyle = {style: 'color:#881391'};
-const alteredValueKeyStyle = {style: 'color:#881391; font-weight: bolder'};
+const keyStyle = {style: 'color: #881391'};
+const defaultValueKeyStyle = {style: 'color: #777'};
+const alteredValueKeyStyle = {style: 'color: #881391; font-weight: bolder'};
+const inlineValuesStyle = {style: 'color: #777; font-style: italic'}
 const nullStyle = {style: 'color: #777'};
 
 export default function createFormatter(Immutable) {
@@ -15,24 +17,56 @@ export default function createFormatter(Immutable) {
     return ['object', {object, config}];
   };
 
-  const displayKeyValueList = (collection) => {
+  const renderIterableHeader = (iterable, name = 'Iterable') =>
+    ['span', ['span', immutableNameStyle, name], ['span', `[${iterable.size}]`]];
+
+  const hasBody = (collection, config) =>
+    collection.size > 0 && !(config && config.noPreview);
+
+  const renderIterableBody = (collection, mapper) => {
     const children = collection
-      .map((value, key) => ['li', reference(key), ': ', reference(value)])
+      .map(mapper)
       .toList()
       .toJS();
     return [ 'ol', listStyle, ...children ];
   }
 
-  const renderIterableHeader = (iterable, name = 'Iterable') =>
-    ['span', ['span', immutableNameStyle, name], ['span', ` (${iterable.size})`]];
-
-  const hasBody = (collection) => collection.size > 0;
-
   const RecordFormatter = {
-    header(record) {
+    header(record, config) {
       if (!(record instanceof Immutable.Record))
         return null;
-      return ['span', immutableNameStyle, record._name || record.constructor.name || 'Record']
+
+      const defaults = record.clear();
+      const changed = !Immutable.is(defaults, record);
+
+      if (config && config.noPreview)
+        return ['span', changed ? immutableNameStyle : nullStyle,
+          record._name || record.constructor.name || 'Record'];
+
+      let inlinePreview;
+      if (!changed) {
+        inlinePreview = ['span', inlineValuesStyle, '{}'];
+      } else {
+        const preview = record
+          .keySeq()
+          .reduce((preview, key) => {
+            if (Immutable.is(defaults.get(key), record.get(key)))
+              return preview;
+            if (preview.length)
+              preview.push(', ');
+
+            preview.push(['span', {},
+              ['span', keyStyle, key + ': '],
+              reference(record.get(key), {noPreview: true})
+            ]);
+            return preview;
+          }, []);
+        inlinePreview = ['span', inlineValuesStyle, '{', ...preview, '}'];
+      }
+      return ['span', {},
+        ['span', immutableNameStyle, record._name || record.constructor.name || 'Record'],
+        ' ', inlinePreview
+      ];
     },
     hasBody,
     body(record) {
@@ -41,7 +75,7 @@ export default function createFormatter(Immutable) {
         .keySeq()
         .map(key => {
           const style = Immutable.is(defaults.get(key), record.get(key))
-            ? keyStyle : alteredValueKeyStyle;
+            ? defaultValueKeyStyle : alteredValueKeyStyle;
           return [
             'li', {},
               ['span', style, key + ': '],
@@ -59,7 +93,11 @@ export default function createFormatter(Immutable) {
       return renderIterableHeader(o, 'List');
     },
     hasBody,
-    body: displayKeyValueList
+    body(o) {
+      return renderIterableBody(o, (value, key) =>
+        ['li', ['span', keyStyle, key + ': '], reference(value)]
+      );
+    }
   };
 
   const StackFormatter = {
@@ -69,7 +107,11 @@ export default function createFormatter(Immutable) {
       return renderIterableHeader(o, 'Stack');
     },
     hasBody,
-    body: displayKeyValueList
+    body(o) {
+      return renderIterableBody(o, (value, key) =>
+        ['li', ['span', keyStyle, key + ': '], reference(value)]
+      );
+    }
   };
 
   const MapFormatter = {
@@ -79,7 +121,11 @@ export default function createFormatter(Immutable) {
       return renderIterableHeader(o, 'Map');
     },
     hasBody,
-    body: displayKeyValueList
+    body(o) {
+      return renderIterableBody(o, (value, key) => 
+        ['li', {}, '{', reference(key), ' => ', reference(value), '}']
+      );
+    }
   };
 
   const OrderedMapFormatter = {
@@ -89,7 +135,11 @@ export default function createFormatter(Immutable) {
       return renderIterableHeader(o, 'OrderedMap');
     },
     hasBody,
-    body: displayKeyValueList
+    body(o) {
+      return renderIterableBody(o, (value, key) => 
+        ['li', {}, '{', reference(key), ' => ', reference(value), '}']
+      );
+    }
   };
 
   const SetFormatter = {
@@ -99,7 +149,11 @@ export default function createFormatter(Immutable) {
       return renderIterableHeader(o, 'Set');
     },
     hasBody,
-    body: displayKeyValueList
+    body(o) {
+      return renderIterableBody(o, value =>
+        ['li', reference(value)]
+      );
+    }
   };
 
   const OrderedSetFormatter = {
@@ -109,7 +163,11 @@ export default function createFormatter(Immutable) {
       return renderIterableHeader(o, 'OrderedSet');
     },
     hasBody,
-    body: displayKeyValueList
+    body(o) {
+      return renderIterableBody(o, value =>
+        ['li', reference(value)]
+      );
+    }
   };
 
   return {
