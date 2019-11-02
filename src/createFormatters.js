4742dc79974e8ae0af228bed311be8e7699fb58a
Dakota Bassett <dakotabassett17@gmail.com>
2019-11-02 00:31:57 -0400

    Formatting

--- src/createFormatters.js
+++ src/createFormatters.js
@@ -95,7 +95,7 @@ export default function createFormatter(Immutable) {
           }, []);
         inlinePreview = ['span', inlineValuesStyle, '{', ...preview, '}'];
       }
-      return ['span',  {},
+      return ['span', {},
         ['span', immutableNameStyle, record._name || record.constructor.name || 'Record'],
         ' ', inlinePreview
       ];
